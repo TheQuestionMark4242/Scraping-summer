18efe0000dc65de1ad6c7769353d3898189b50b1
TheQuestionMark4242 <73532221+TheQuestionMark4242@users.noreply.github.com>
2021-05-18 17:20:32 +0530

    Update Scraper.py
    Fixed commas in title error

--- Scraper.py
+++ Scraper.py
@@ -3,17 +3,14 @@ import requests
 import json
 webpage = "https://summerofcode.withgoogle.com/api/program/current/project/?page=1&page_size=20https://summerofcode.withgoogle.com/projects/"
 f = open("gsoc2021.csv","w")
-counter = 1
+f.write("Name,Project,Organization")
 while(webpage):
     page = requests.get(webpage)
     soup = BeautifulSoup(page.content, 'html.parser')
     site_json = json.loads(soup.text)
     results = site_json['results']
     for i in results:
-        f.write(i["student"]["display_name"]+",\""+i["title"]+"\", "+i["organization"]["name"]+"\n")
+        f.write(i["student"]["display_name"]+",\""+i["title"].replace('"', '""')+"\",\""+i["organization"]["name"]+"\"\n")
     webpage = site_json['next']
-    print(counter)
-    counter+=1
 f.close
-
 
