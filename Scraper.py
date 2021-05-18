70e58bc0d423716c0c58d21729bca1746342c0b3
TheQuestionMark4242 <73532221+TheQuestionMark4242@users.noreply.github.com>
2021-05-18 17:22:46 +0530

    Update Scraper.py

--- Scraper.py
+++ Scraper.py
@@ -3,7 +3,7 @@ import requests
 import json
 webpage = "https://summerofcode.withgoogle.com/api/program/current/project/?page=1&page_size=20https://summerofcode.withgoogle.com/projects/"
 f = open("gsoc2021.csv","w")
-f.write("Name,Project,Organization")
+f.write("Name,Project,Organization\n")
 while(webpage):
     page = requests.get(webpage)
     soup = BeautifulSoup(page.content, 'html.parser')
